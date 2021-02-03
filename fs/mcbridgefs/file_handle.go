d324be1299666146cb4eb28744a3d58b30c67276
gtarcea <glenn.tarcea@gmail.com>
2021-02-03 11:44:00 -0500

    Create the mcbridgefs version of the file handle, right now its just a pass through to the bridge file handle

--- fs/mcbridgefs/file_handle.go
+++ fs/mcbridgefs/file_handle.go
@@ -1,227 +1,30 @@
 package mcbridgefs
 
 import (
-	"context"
 	"github.com/hanwen/go-fuse/v2/fs"
-	"golang.org/x/sys/unix"
-	"sync"
-	"syscall"
-	"time"
-	"unsafe"
-
-	"github.com/hanwen/go-fuse/v2/fuse"
+	"github.com/materials-commons/mcbridgefs/fs/bridgefs"
 )
 
 // Code based on loopback file system from github.com/hanwen/go-fuse/v2/fs/file.go
 
 type FileHandle struct {
-	mu sync.Mutex
-	fd int
+	*bridgefs.BridgeFileHandle
 }
 
-func (f *FileHandle) Read(ctx context.Context, buf []byte, off int64) (res fuse.ReadResult, errno syscall.Errno) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	r := fuse.ReadResultFd(uintptr(f.fd), off, len(buf))
-	return r, fs.OK
-}
+var _ = (fs.FileHandle)((*FileHandle)(nil))
+var _ = (fs.FileReleaser)((*FileHandle)(nil))
+var _ = (fs.FileGetattrer)((*FileHandle)(nil))
+var _ = (fs.FileReader)((*FileHandle)(nil))
+var _ = (fs.FileWriter)((*FileHandle)(nil))
+var _ = (fs.FileGetlker)((*FileHandle)(nil))
+var _ = (fs.FileSetlker)((*FileHandle)(nil))
+var _ = (fs.FileSetlkwer)((*FileHandle)(nil))
+var _ = (fs.FileLseeker)((*FileHandle)(nil))
+var _ = (fs.FileFlusher)((*FileHandle)(nil))
+var _ = (fs.FileFsyncer)((*FileHandle)(nil))
+var _ = (fs.FileSetattrer)((*FileHandle)(nil))
+var _ = (fs.FileAllocater)((*FileHandle)(nil))
 
-func (f *FileHandle) Write(ctx context.Context, data []byte, off int64) (uint32, syscall.Errno) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	n, err := syscall.Pwrite(f.fd, data, off)
-	return uint32(n), fs.ToErrno(err)
-}
-
-func (f *FileHandle) Release(ctx context.Context) syscall.Errno {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	if f.fd != -1 {
-		err := syscall.Close(f.fd)
-		f.fd = -1
-		return fs.ToErrno(err)
-	}
-	return syscall.EBADF
-}
-
-func (f *FileHandle) Flush(ctx context.Context) syscall.Errno {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	// Since Flush() may be called for each dup'd fd, we don't
-	// want to really close the file, we just want to flush. This
-	// is achieved by closing a dup'd fd.
-	newFd, err := syscall.Dup(f.fd)
-
-	if err != nil {
-		return fs.ToErrno(err)
-	}
-	err = syscall.Close(newFd)
-	return fs.ToErrno(err)
-}
-
-func (f *FileHandle) Fsync(ctx context.Context, flags uint32) (errno syscall.Errno) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	r := fs.ToErrno(syscall.Fsync(f.fd))
-
-	return r
-}
-
-const (
-	_OFD_GETLK  = 36
-	_OFD_SETLK  = 37
-	_OFD_SETLKW = 38
-)
-
-func (f *FileHandle) Getlk(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32, out *fuse.FileLock) (errno syscall.Errno) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	flk := syscall.Flock_t{}
-	lk.ToFlockT(&flk)
-	errno = fs.ToErrno(syscall.FcntlFlock(uintptr(f.fd), _OFD_GETLK, &flk))
-	out.FromFlockT(&flk)
-	return
-}
-
-func (f *FileHandle) Setlk(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32) (errno syscall.Errno) {
-	return f.setLock(ctx, owner, lk, flags, false)
-}
-
-func (f *FileHandle) Setlkw(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32) (errno syscall.Errno) {
-	return f.setLock(ctx, owner, lk, flags, true)
-}
-
-func (f *FileHandle) setLock(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32, blocking bool) (errno syscall.Errno) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	if (flags & fuse.FUSE_LK_FLOCK) != 0 {
-		var op int
-		switch lk.Typ {
-		case syscall.F_RDLCK:
-			op = syscall.LOCK_SH
-		case syscall.F_WRLCK:
-			op = syscall.LOCK_EX
-		case syscall.F_UNLCK:
-			op = syscall.LOCK_UN
-		default:
-			return syscall.EINVAL
-		}
-		if !blocking {
-			op |= syscall.LOCK_NB
-		}
-		return fs.ToErrno(syscall.Flock(f.fd, op))
-	} else {
-		flk := syscall.Flock_t{}
-		lk.ToFlockT(&flk)
-		var op int
-		if blocking {
-			op = _OFD_SETLKW
-		} else {
-			op = _OFD_SETLK
-		}
-		return fs.ToErrno(syscall.FcntlFlock(uintptr(f.fd), op, &flk))
-	}
-}
-
-func (f *FileHandle) Setattr(ctx context.Context, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
-	if errno := f.setAttr(ctx, in); errno != 0 {
-		return errno
-	}
-
-	return f.Getattr(ctx, out)
-}
-
-func (f *FileHandle) setAttr(ctx context.Context, in *fuse.SetAttrIn) syscall.Errno {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	var errno syscall.Errno
-	if mode, ok := in.GetMode(); ok {
-		errno = fs.ToErrno(syscall.Fchmod(f.fd, mode))
-		if errno != 0 {
-			return errno
-		}
-	}
-
-	uid32, uOk := in.GetUID()
-	gid32, gOk := in.GetGID()
-	if uOk || gOk {
-		uid := -1
-		gid := -1
-
-		if uOk {
-			uid = int(uid32)
-		}
-		if gOk {
-			gid = int(gid32)
-		}
-		errno = fs.ToErrno(syscall.Fchown(f.fd, uid, gid))
-		if errno != 0 {
-			return errno
-		}
-	}
-
-	mtime, mok := in.GetMTime()
-	atime, aok := in.GetATime()
-
-	if mok || aok {
-		ap := &atime
-		mp := &mtime
-		if !aok {
-			ap = nil
-		}
-		if !mok {
-			mp = nil
-		}
-		errno = f.utimens(ap, mp)
-		if errno != 0 {
-			return errno
-		}
-	}
-
-	if sz, ok := in.GetSize(); ok {
-		errno = fs.ToErrno(syscall.Ftruncate(f.fd, int64(sz)))
-		if errno != 0 {
-			return errno
-		}
-	}
-	return fs.OK
-}
-
-func (f *FileHandle) Getattr(ctx context.Context, a *fuse.AttrOut) syscall.Errno {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	st := syscall.Stat_t{}
-	err := syscall.Fstat(f.fd, &st)
-	if err != nil {
-		return fs.ToErrno(err)
-	}
-	a.FromStat(&st)
-
-	return fs.OK
-}
-
-func (f *FileHandle) Lseek(ctx context.Context, off uint64, whence uint32) (uint64, syscall.Errno) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	n, err := unix.Seek(f.fd, int64(off), int(whence))
-	return uint64(n), fs.ToErrno(err)
-}
-
-// Utimens - file handle based version of loopbackFileSystem.Utimens()
-func (f *FileHandle) utimens(a *time.Time, m *time.Time) syscall.Errno {
-	var ts [2]syscall.Timespec
-	ts[0] = fuse.UtimeToTimespec(a)
-	ts[1] = fuse.UtimeToTimespec(m)
-	err := futimens(int(f.fd), &ts)
-	return fs.ToErrno(err)
-}
-
-// futimens - futimens(3) calls utimensat(2) with "pathname" set to null and
-// "flags" set to zero
-func futimens(fd int, times *[2]syscall.Timespec) (err error) {
-	_, _, e1 := syscall.Syscall6(syscall.SYS_UTIMENSAT, uintptr(fd), 0, uintptr(unsafe.Pointer(times)), uintptr(0), 0, 0)
-	if e1 != 0 {
-		err = syscall.Errno(e1)
-	}
-	return
+func NewFileHandle(fd int) fs.FileHandle {
+	return &FileHandle{bridgefs.NewBridgeFileHandle(fd).(*bridgefs.BridgeFileHandle)}
 }

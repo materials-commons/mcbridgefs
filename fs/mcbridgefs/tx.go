990c13f17efd4f19b23faa38dc037f9579c6b3fc
gtarcea <glenn.tarcea@gmail.com>
2021-02-10 12:03:01 -0500

    Default txRetryCount to 3 if there is an error or if the retry count is set to less than 3

--- fs/mcbridgefs/tx.go
+++ fs/mcbridgefs/tx.go
@@ -4,8 +4,13 @@ import "gorm.io/gorm"
 
 func withTxRetry(fn func(tx *gorm.DB) error, db *gorm.DB, retryCount int) error {
 	var err error
+
+	if retryCount < 3 {
+		retryCount = 3
+	}
+
 	for i := 0; i < retryCount; i++ {
-		err = DB.Transaction(fn)
+		err = db.Transaction(fn)
 		if err == nil {
 			break
 		}

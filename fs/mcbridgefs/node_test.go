d6585fec1d89750e7f2b54452e1d4919f5e62770
gtarcea <glenn.tarcea@gmail.com>
2021-02-10 12:04:27 -0500

    System level hard coded tests

--- fs/mcbridgefs/node_test.go
+++ fs/mcbridgefs/node_test.go
@@ -27,6 +27,27 @@ func TestWrite100ThousandFiles(t *testing.T) {
 	}
 }
 
+func TestWrite5000FilesInParallel(t *testing.T) {
+	timeStart := time.Now()
+	for i := 0; i < 50; i++ {
+		t.Run(fmt.Sprintf("Parallel%d", i), func(t *testing.T) {
+			t.Parallel()
+			for j := 0; j < 100; j++ {
+				fname, err := uuid.GenerateUUID()
+				require.NoError(t, err, "GenerateUUID failed: %s", err)
+				filename := filepath.Join("/home/gtarcea/mcdir/mcfs/data/test/tproj", fname+".txt")
+				err = ioutil.WriteFile(filename, []byte(fname), 0644)
+				require.NoError(t, err, "ioutil.WriteFile(%s) failed: %s", fname, err)
+				if j%10 == 0 {
+					timeElapsed := time.Now().Sub(timeStart)
+					fmt.Printf("Wrote %d files in %.0f seconds...\n", i, timeElapsed.Seconds())
+					timeStart = time.Now()
+				}
+			}
+		})
+	}
+}
+
 func TestFTruncateFile(t *testing.T) {
 	fd, err := syscall.Open("/home/gtarcea/mcdir/mcfs/data/test/tproj/newfile.txt", syscall.O_RDWR, 0)
 	require.NoError(t, err, "Open failed: %s", err)

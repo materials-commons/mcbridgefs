d76eeb25611c71e3de7365877d7cb7538154fd43
gtarcea <glenn.tarcea@gmail.com>
2021-02-19 15:05:47 -0500

    Start working out how we could support a single MCBridgeFS instance

---
+++ fs/mcbridgefs/path.go
@@ -0,0 +1,60 @@
+package mcbridgefs
+
+import (
+	"fmt"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func t() {
+	path := "/gtarcea@umich.edu/1/rest/of/path"
+	fmt.Printf("%q\n", strings.SplitN(path, "/", 4))
+	fmt.Printf("%s: %+v\n", "/gtarcea@umich.edu/1", ToPath("/gtarcea@umich.edu/1"))
+	fmt.Printf("%s: %+v\n", "/gtarcea@umich.edu/1/abc", ToPath("/gtarcea@umich.edu/1/abc"))
+	fmt.Printf("%s: %+v\n", "/gtarcea@umich.edu/1/abc/def/ghi.txt", ToPath("/gtarcea@umich.edu/1/abc/def/ghi.txt"))
+	fmt.Printf("%s: %+v\n", "/gtarcea@umich.edu", ToPath("/gtarcea@umich.edu"))
+}
+
+type Path struct {
+	Email     string
+	ProjectID int
+	Path      string
+}
+
+func (p *Path) IsRoot() bool {
+	return p.Email == ""
+}
+
+func (p *Path) IsEmail() bool {
+	return p.Email != ""
+}
+
+func (p *Path) IsProject() bool {
+	return p.ProjectID != 0
+}
+
+func (n *Node) ToPath() *Path {
+	basePath := n.Path(n.Root())
+	return ToPath(filepath.Join("/", basePath))
+}
+
+func ToPath(p string) *Path {
+	pathParts := strings.SplitN(p, "/", 4)
+
+	id := 0
+	if len(pathParts) > 2 {
+		id, _ = strconv.Atoi(pathParts[2])
+	}
+
+	rest := "/"
+	if len(pathParts) == 4 {
+		rest = filepath.Join("/", pathParts[3])
+	}
+
+	return &Path{
+		Email:     pathParts[1],
+		ProjectID: id,
+		Path:      rest,
+	}
+}

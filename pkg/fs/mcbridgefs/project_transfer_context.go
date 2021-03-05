b9b1fd292511f255f4c80a3f92330cecd98b78d2
gtarcea <glenn.tarcea@gmail.com>
2021-03-05 16:09:47 -0500

    WIP - project transfer context for an existing or new transfer     * Setup globus     * Differentiate between different types of transfers (globus vs others...)

--- pkg/fs/mcbridgefs/project_transfer_context.go
+++ pkg/fs/mcbridgefs/project_transfer_context.go
@@ -1,10 +1,12 @@
 package mcbridgefs
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"sync"
 
+	"github.com/apex/log"
 	"github.com/hashicorp/go-uuid"
 	globus "github.com/materials-commons/goglobus"
 	"github.com/materials-commons/gomcdb/mcmodel"
@@ -28,8 +30,8 @@ var projectTransfers sync.Map
 var globusClient *globus.Client
 
 func setupTransferContext(pathContext TransferPathContext) error {
-	projectTransferContext := ProjectTransferContext{}
-	val, ok := projectTransfers.LoadOrStore(pathContext.ProjectPathContext(), &projectTransferContext)
+	projectTransferContext := &ProjectTransferContext{}
+	val, ok := projectTransfers.LoadOrStore(pathContext.ProjectPathContext(), projectTransferContext)
 	if ok {
 		// There was already a ProjectTransferRequest loaded
 		projectContext := val.(*ProjectTransferContext)
@@ -53,27 +55,35 @@ func setupTransferContext(pathContext TransferPathContext) error {
 		return nil
 	}
 
-	// If we are here then we are creating the transfer, which means setting up the ACL and all the
-	// other context.
-
-	// Create Transfer Request
-	transferRequest, _ := createTransferRequest(pathContext.ProjectID, pathContext.UserID)
-	_ = transferRequest
-
-	rule := globus.EndpointACLRule{
-		PrincipalType: globus.ACLPrincipalTypeIdentity,
-		EndpointID:    os.Getenv("MC_GLOBUS_ENDPOINT_ID"),
-		Path:          fmt.Sprintf("/__transfers/%d/%d/", pathContext.UserID, pathContext.ProjectID),
-		IdentityID:    "",
-		Permissions:   "rw",
-	}
-	aclRes, err := globusClient.AddEndpointACLRule(rule)
-	if err != nil {
+	// if this is a globus transfer we need to validate that the user has a globus user account setup
+	var user mcmodel.User
+	if err := db.First(&user, pathContext.UserID).Error; err != nil {
 		return err
 	}
-	projectContext.globusContext.globusACL = aclRes.AccessID
+
+	if user.GlobusUser == "" {
+		// This user hasn't configured a globus account so we can't setup this transfer request
+		return errors.New("no globus account configured")
+	}
+
+	// If we are here then we are creating the transfer. We will also look at the
+	// transfer type in the
+
+	var err error
+	if projectContext.transferRequest, err = createTransferRequest(pathContext.ProjectID, pathContext.UserID); err != nil {
+		return err
+	}
+
+	if pathContext.TransferType == "globus" {
+		if err := setupGlobus(pathContext, projectContext, user); err != nil {
+			return err
+		}
+	}
+
 	projectContext.setupComplete = true
-	projectTransfers.Store(pathContext.ProjectPathContext(), projectContext)
+
+	// We don't need to store the updated projectContext because projectContext is a pointer to the
+	// context stored in the projectTransfers.
 
 	return nil
 }
@@ -94,4 +104,53 @@ func createTransferRequest(projectID, userID int) (mcmodel.TransferRequest, erro
 	result := db.Create(&transferRequest)
 
 	return transferRequest, result.Error
+}
+
+func setupGlobus(pathContext TransferPathContext, projectContext *ProjectTransferContext, user mcmodel.User) error {
+	identities, err := globusClient.GetIdentities([]string{user.GlobusUser})
+	if err != nil {
+		return nil
+	}
+
+	projectContext.globusContext.globusIdentityID = identities.Identities[0].ID
+
+	rule := globus.EndpointACLRule{
+		PrincipalType: globus.ACLPrincipalTypeIdentity,
+		EndpointID:    os.Getenv("MC_GLOBUS_ENDPOINT_ID"),
+		Path:          fmt.Sprintf("/__transfers/%d/%d/", pathContext.UserID, pathContext.ProjectID),
+		IdentityID:    projectContext.globusContext.globusIdentityID,
+		Permissions:   "rw",
+	}
+	aclRes, err := globusClient.AddEndpointACLRule(rule)
+	if err != nil {
+		return err
+	}
+	projectContext.globusContext.globusACL = aclRes.AccessID
+
+	return nil
+}
+
+func cleanupTransferContext(pathContext TransferPathContext) {
+	val, ok := projectTransfers.Load(pathContext.ProjectPathContext())
+	if !ok {
+		// Not in the store any longer
+		return
+	}
+
+	transferContext := val.(*ProjectTransferContext)
+	transferContext.mutex.Lock()
+	defer transferContext.mutex.Unlock()
+
+	endpointID := os.Getenv("MC_GLOBUS_ENDPOINT_ID")
+	if _, err := globusClient.DeleteEndpointACLRule(endpointID, transferContext.globusContext.globusACL); err != nil {
+		log.Errorf("Failed deleting ACL for %s: %s", pathContext.ProjectPathContext(), err)
+	}
+
+	if err := db.Delete(&transferContext.transferRequest).Error; err != nil {
+		log.Errorf("Failed to delete TransferRequest %d: %s", transferContext.transferRequest.ID, err)
+	}
+
+	projectTransfers.Delete(pathContext.ProjectPathContext())
+
+	return
 }

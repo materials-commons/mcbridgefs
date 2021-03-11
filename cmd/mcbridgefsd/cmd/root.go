230a4bcf6c3133269560143bd8ce8288f7f26656
gtarcea <glenn.tarcea@gmail.com>
2021-03-11 15:22:22 -0500

    Cleanup server use, short and long descriptions

--- cmd/mcbridgefsd/cmd/root.go
+++ cmd/mcbridgefsd/cmd/root.go
@@ -48,16 +48,9 @@ type ActiveBridge struct {
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "mcbridefsd",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Use:   "mcbridgefsd",
+	Short: "Server for launching bridges",
+	Long:  `The mcbridgefsd is responsible for launching new mcbridgefs and monitoring if they exit prematurely.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		e := echo.New()
 		e.HideBanner = true

c1c7143650ec7fe2ac412416a6d90994e27d2b12
gtarcea <glenn.tarcea@gmail.com>
2021-12-14 13:31:00 -0500

    Update unit test to actually check if expired

--- pkg/monitor/activity_monitor_test.go
+++ pkg/monitor/activity_monitor_test.go
@@ -1,22 +1,17 @@
 package monitor
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
 
 func TestOneWeekAfterLogic(t *testing.T) {
-	now := time.Now()
+	now := time.Now().Add(2 * oneWeek)
 
 	lastChanged := time.Date(2021, 12, 13, 0, 0, 0, 0, time.UTC)
-
 	oneWeekSinceLastChanged := lastChanged.Add(oneWeek)
-	nowLongerThanOneWeek := now.Add(2 * oneWeek)
 
-	if nowLongerThanOneWeek.After(oneWeekSinceLastChanged) {
-		fmt.Println("expired")
-	} else {
-		fmt.Println("not expired")
+	if !now.After(oneWeekSinceLastChanged) {
+		t.Fatalf("Should have been expired")
 	}
 }
